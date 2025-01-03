0c413c373c1396cb003e9808dc8d48ebb76af86c
JUAN_HUN <juanhun2424@gmail.com>
2025-01-03 17:10:02 -0500

    route the final urlpatterns in the main project

--- backend/redzone_getaway_proj/urls.py
+++ backend/redzone_getaway_proj/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('api/v1/users/', include('user_app.urls')),
     path('api/v1/games/', include('game_app.urls')),
     path('api/v1/events/', include('event_app.urls')),
+    path('api/v1/flights/', include('flight_app.urls')),
+    path('api/v1/hotels/', include('hotel_app.urls')),
     path('api/v1/locations/', include('location_app.urls')),
     path('api/v1/teams/', include('team_app.urls')),
     path('api/v1/api_app/', include('api_app.urls')),
